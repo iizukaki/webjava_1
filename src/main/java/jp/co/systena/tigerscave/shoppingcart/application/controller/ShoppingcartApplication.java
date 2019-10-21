f67380684bb2990b23d183742756ca43da3c3f98
Systena <Systena@DESKTOP-4D0DV3C>
2019-10-21 19:47:48 +0900

    applicatoion追加

---
+++ src/main/java/jp/co/systena/tigerscave/shoppingcart/application/controller/ShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.shoppingcart.application.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartApplication.class, args);
+	}
+
+}
